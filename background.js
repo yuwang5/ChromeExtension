74b2c9352d81ecdbebbbd0b4639d413d1902ba6e
asmwritercmu <98866781+asmwritercmu@users.noreply.github.com>
2022-04-04 15:14:30 +0530

    Fixed bugs

--- background.js
+++ background.js
@@ -4,62 +4,64 @@ var customHeaders;
 var headerInjectionEnable;
 
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set({ websiteList: new Set() });
-    chrome.storage.sync.set({ currentURL: "" });
+    chrome.storage.sync.set({ websiteList:new Set() });
+    chrome.storage.sync.set({ currentURL: ""});
+	
 });
+
 
 /*
 * Add listener for onBeforeSendHeaders events which provides access to headers.
 */
-chrome.webRequest.onBeforeSendHeaders.addListener(function (info) {
+chrome.webRequest.onBeforeSendHeaders.addListener(function(info) {
 
-    // Get already saved value of 'Enable' checkbox.
-    chrome.storage.sync.get('headerInjectionEnable', function (items) {
-        if (typeof items.headerInjectionEnable != "undefined") {
-            headerInjectionEnable = items.headerInjectionEnable;
-        }
-    });
-
-    // If 'Enable' checkbox is checked, then only add header.
-    if (headerInjectionEnable) {
-        // Get already saved value of header JSON.
-        chrome.storage.sync.get('headerJson', function (items) {
-            if (typeof items.headerJson != "undefined") {
-                customHeaders = items.headerJson;
+		// Get already saved value of 'Enable' checkbox.
+        chrome.storage.sync.get('headerInjectionEnable', function(items) {
+            if (typeof items.headerInjectionEnable != "undefined") {
+                headerInjectionEnable = items.headerInjectionEnable;
             }
         });
 
-        if (customHeaders) {
-            // Iterate over headers from JSON. If its new, then add header. 
-            // If header is existing, then update value.
-            for (var i = 0; i < customHeaders.length; i++) {
-                var headerObj = customHeaders[i];
-                var existing = false;
+		// If 'Enable' checkbox is checked, then only add header.
+        if (headerInjectionEnable) {
+			//alert("headers added");
+			// Get already saved value of header JSON.
+            chrome.storage.sync.get('headerJson', function(items) {
+                if (typeof items.headerJson != "undefined") {
+                    customHeaders = items.headerJson;
+                }
+            });
 
-                info.requestHeaders.forEach(function (header, i) {
-                    console.log("header - " + headerObj.name.toLowerCase());
-                    if (header.name.toLowerCase() == headerObj.name.toLowerCase()) {
-                        header.value = headerObj.value;
-                        existing = true;
-                    }
-                });
-
-                if (!existing) {
-                    info.requestHeaders.push({
-                        name: headerObj.name,
-                        value: headerObj.value
+            if (customHeaders) {
+				// Iterate over headers from JSON. If its new, then add header. 
+				// If header is existing, then update value.
+                for (var i = 0; i < customHeaders.length; i++) {
+                    var headerObj = customHeaders[i];
+                    var existing = false;
+					
+                    info.requestHeaders.forEach(function(header, i) {
+                        console.log("header - " + headerObj.name.toLowerCase());
+                        if (header.name.toLowerCase() == headerObj.name.toLowerCase()) {
+                            header.value = headerObj.value;
+                            existing = true;
+                        }
                     });
+                    if (!existing) {
+                        info.requestHeaders.push({
+                            name: headerObj.name,
+                            value: headerObj.value
+                        });
+                    }
                 }
             }
         }
-    }
 
-    // Return updated header list.
-    return {
-        requestHeaders: info.requestHeaders
-    };
-},
-    // Do this for all URLs
+		// Return updated header list.
+        return {
+            requestHeaders: info.requestHeaders
+        };
+    },
+	// Do this for all URLs
     {
         urls: ["<all_urls>"]
     },
--- popup.js
+++ popup.js
@@ -3,20 +3,29 @@
 // document.getElementById('main').style.height = '300px';
 var background = chrome.extension.getBackgroundPage();
 
+
 // Add a callback when HTML is loaded.
 document.addEventListener('DOMContentLoaded', domLoadedCallback, false);
 // This is a callback method once HTML is loaded. 
 function domLoadedCallback() {
     // get the origin url 
-    chrome.storage.sync.get('headerJson', function (items) {
-        if (typeof items.headerJson != "undefined") {
-            document.getElementById("changePreference").innerHTML = "Opt out";
-        }
-        else {
+	chrome.storage.sync.get('headerInjectionEnable', function(items) {
+        if (items.headerInjectionEnable == true) {
+			navigator.globalPrivacyControl = true;
             document.getElementById("changePreference").innerHTML = "Opt in";
+			document.getElementById("newPreference").innerHTML = "Updated " + getUserPreference();
         }
+		else{
+			navigator.globalPrivacyControl = false;
+			document.getElementById("changePreference").innerHTML = "Opt out";
+			document.getElementById("newPreference").innerHTML = "Updated " + getUserPreference();
+
+		}
     });
-    displayList();
+	/*
+	
+	*/
+	displayList();
 }
 
 function rewriteUserAgentHeader(e) {
@@ -24,30 +33,30 @@ function rewriteUserAgentHeader(e) {
     gpc_header.name = "gpc";
     gpc_header.value = "true";
     e.requestHeaders.push(gpc_header);
-    e.requestHeaders.forEach(function (header) {
-        if (header.name.toLowerCase() == "gpc") {
-            header.value = "true";
-            //alert("Has conformed to GPC");
-        }
+    e.requestHeaders.forEach(function(header){
+      if (header.name.toLowerCase() == "gpc") {
+        header.value = "true";
+        //alert("Has conformed to GPC");
+      }
     });
 
 }
 
 function getGpcResult(url) {
     const gpcSuffix = ".well-known/gpc.json";
-    jQuery.getJSON(url + gpcSuffix, function (data) {
+    jQuery.getJSON(url + gpcSuffix, function(data) {
         if (data.gpc == true) {
             document.getElementById("gpc").innerHTML = "Website abides to GPC";
         } else {
             document.getElementById("gpc").innerHTML = "Website does not abides to GPC";
         }
     })
-        .fail(function () {
-            document.getElementById("gpc").innerHTML = "Website does not have GPC configuration";
-        });
+    .fail(function() {
+        document.getElementById("gpc").innerHTML = "Website does not have GPC configuration";
+    });
 }
 
-chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
+chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
     let tabURL = tabs[0].url;
     background.currentURL = tabURL;
     document.getElementById("greeting").innerHTML = tabURL;
@@ -56,26 +65,27 @@ chrome.tabs.query({ active: true, lastFocusedWindow: true }, tabs => {
 
 function getUserPreference() {
     if (!navigator.globalPrivacyControl) {
-        return "user GPC peference: false";
+        return "user GPC preference: false";
     } else {
         return "user GPC preference: true";
     }
 }
-
+/*
 document.getElementById("userPreference").innerHTML = "Initial " + getUserPreference();
 // Initial button text display
 if (!navigator.globalPrivacyControl) {
-    document.getElementById("changePreference").innerHTML = "Opt in";
-} else {
     document.getElementById("changePreference").innerHTML = "Opt out";
+} else {
+    document.getElementById("changePreference").innerHTML = "Opt in";
 }
+*/
 
 function displayList() {
     let list = document.getElementById("websiteList");
     while (list.firstChild) {
         list.removeChild(list.firstChild);
     }
-    background.websiteList.forEach((item) => {
+    background.websiteList.forEach((item)=>{
         let li = document.createElement("li");
         li.innerText = item;
         list.appendChild(li);
@@ -84,19 +94,20 @@ function displayList() {
 
 function onclickFunction() {
     if (!navigator.globalPrivacyControl) {
-        setHeaders();
+		
+		setHeaders();
         navigator.globalPrivacyControl = true;
-        document.getElementById("changePreference").innerHTML = "Opt out";
-        if (background.websiteList == undefined) {
+        document.getElementById("changePreference").innerHTML = "Opt in";
+        if(background.websiteList == undefined){
             background.websiteList = new Set();
         }
         background.websiteList.add(background.currentURL);
         displayList();
     } else {
-        removeHeaders();
+		removeHeaders();
         navigator.globalPrivacyControl = false;
-        document.getElementById("changePreference").innerHTML = "Opt in";
-        if (background.websiteList == undefined) {
+        document.getElementById("changePreference").innerHTML = "Opt out";
+        if(background.websiteList == undefined){
             background.websiteList = new Set();
         }
         background.websiteList.delete(background.currentURL);
@@ -108,7 +119,7 @@ function onclickFunction() {
 document.getElementById("changePreference").addEventListener("click", onclickFunction);
 
 function setHeaders() {
-    alert("setheaders");
+	//alert("setheaders");
     chrome.storage.sync.set({
         'headerJson': [{ "name": "Sec-GPC", "value": "1" }]
     }, function () { });
@@ -118,7 +129,7 @@ function setHeaders() {
 }
 
 function removeHeaders() {
-    alert("Removed headers");
+	//alert("Removed headers");
     chrome.storage.sync.set({
         'headerJson': undefined
     }, function () { });
